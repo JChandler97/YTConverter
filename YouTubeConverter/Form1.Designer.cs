f88c9dea03cce5302f8240537414c9f4a79495e1
james <james@DESKTOP-OU8FN2P>
2024-04-18 02:58:42 -0400

    Add project files.

---
+++ YouTubeConverter/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace YouTubeConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelCheck = new System.Windows.Forms.Label();
+            this.DLButton = new System.Windows.Forms.Button();
+            this.FPDisplay = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.FileButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(293, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Enter YouTube link here";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(498, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Check Link";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelCheck
+            // 
+            this.labelCheck.AutoSize = true;
+            this.labelCheck.Location = new System.Drawing.Point(641, 81);
+            this.labelCheck.Name = "labelCheck";
+            this.labelCheck.Size = new System.Drawing.Size(46, 17);
+            this.labelCheck.TabIndex = 3;
+            this.labelCheck.Text = "label2";
+            this.labelCheck.Visible = false;
+            // 
+            // DLButton
+            // 
+            this.DLButton.Location = new System.Drawing.Point(627, 226);
+            this.DLButton.Name = "DLButton";
+            this.DLButton.Size = new System.Drawing.Size(109, 23);
+            this.DLButton.TabIndex = 4;
+            this.DLButton.Text = "Download";
+            this.DLButton.UseVisualStyleBackColor = true;
+            this.DLButton.Visible = false;
+            this.DLButton.Click += new System.EventHandler(this.DLButton_Click);
+            // 
+            // FPDisplay
+            // 
+            this.FPDisplay.Location = new System.Drawing.Point(293, 226);
+            this.FPDisplay.Name = "FPDisplay";
+            this.FPDisplay.ReadOnly = true;
+            this.FPDisplay.Size = new System.Drawing.Size(292, 22);
+            this.FPDisplay.TabIndex = 5;
+            this.FPDisplay.Text = "Select a folder:";
+            // 
+            // FileButton
+            // 
+            this.FileButton.Location = new System.Drawing.Point(591, 225);
+            this.FileButton.Name = "FileButton";
+            this.FileButton.Size = new System.Drawing.Size(30, 23);
+            this.FileButton.TabIndex = 7;
+            this.FileButton.Text = "...";
+            this.FileButton.UseVisualStyleBackColor = true;
+            this.FileButton.Click += new System.EventHandler(this.FileButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(953, 526);
+            this.Controls.Add(this.FileButton);
+            this.Controls.Add(this.FPDisplay);
+            this.Controls.Add(this.DLButton);
+            this.Controls.Add(this.labelCheck);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Youtube Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelCheck;
+        private System.Windows.Forms.Button DLButton;
+        private System.Windows.Forms.TextBox FPDisplay;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button FileButton;
+    }
+}
+
---
+++ YouTubeConverter/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VideoLibrary;
+using MediaToolkit;
+using MediaToolkit.Model;
+
+namespace YouTubeConverter
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = textBox1.Text;
+            LinkCheck(url);
+        }
+
+        private bool LinkCheck(string url)
+        {
+            if (url.Contains("youtube.com/watch?v=") || url.Contains("youtu.be/"))
+            {
+                labelCheck.ForeColor = Color.Green;
+                labelCheck.MaximumSize = new Size(200, 0);
+                labelCheck.Text = "Link works! Proceed to download!";
+                labelCheck.Visible = true;
+                DLButton.Visible = true;
+                return true;
+            }
+            else
+            {
+                labelCheck.ForeColor = Color.Red;
+                labelCheck.MaximumSize = new Size(100, 0);
+                labelCheck.AutoSize = true;
+                labelCheck.Text = "Link does not work! Please try again!";
+                labelCheck.Visible = true;
+                DLButton.Visible = false;
+                return false;
+            }
+        }
+
+        private void FileButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+
+            if(folder.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                FPDisplay.Text = folder.SelectedPath;
+            }
+        }
+
+        private void VideoDownload(string url)
+        {
+            string filePath = FPDisplay.Text;
+
+
+                if (LinkCheck(url) == true)
+                {
+                    var youtube = YouTube.Default;
+                    var video = youtube.GetVideo(url);
+
+                //Checks to see if any invalid characters in the path name
+                //Since the API doesn't know any better, we'll have to create a variable for the video title to do so
+                var videoName = video.Title;
+
+                foreach(char c in System.IO.Path.GetInvalidFileNameChars())
+                    {
+                        videoName = videoName.Replace(c, ' ');
+                    }
+
+                    //ew yuck below
+                    string trueFileName = filePath + @"\" + videoName;
+                        
+                    // Just a test to check if filepath is actually good to go
+                    //FPDisplay.Text = trueFileName;
+
+                    byte[] bytes = video.GetBytes();
+
+                    //Need to figure out what Stream() actually does and if it's useful for the actual program
+                    //You may not even be able to even need Stream() regardless
+                    //var stream = video.Stream();
+
+                    File.WriteAllBytes(@trueFileName, video.GetBytes());
+
+                        //Actual conversion HERE
+                        var inputFile = new MediaFile { Filename = trueFileName };
+                        var outputFile = new MediaFile { Filename = $"{trueFileName}.mp3" };
+
+                    using(var engine = new Engine())
+                    {
+                        engine.GetMetadata(inputFile);
+                        engine.Convert(inputFile, outputFile);
+                    }
+
+                //Cleanup and Confirmation
+                File.Delete(Path.Combine(filePath, videoName));
+                DialogResult result = MessageBox.Show("Your file is now complete!", "Confirmation");
+                GC.Collect();
+                }
+
+                else
+                {
+                    DialogResult result = MessageBox.Show("Link does not work! Try again.", "Warning");
+                }
+
+        }
+
+        private void DLButton_Click(object sender, EventArgs e)
+        {
+            string url = textBox1.Text;
+            string filePath = FPDisplay.Text;
+
+            if (Directory.Exists(filePath))
+            {
+                VideoDownload(url);
+            }
+            else
+            {
+                DialogResult result = MessageBox.Show("Please choose a valid location for your file.", "Warning");
+            }
+
+        }
+    }
+}
